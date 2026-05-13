693e71fd21202460007a7e8cdc683bcc7a52514e
VishnuVardhaThorupunuri <vishuthorupunuri@gmail.com>
2026-05-13 11:29:17 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ config(); //process.env
 //Create express application
 const app = exp();
 //use cors middleware
-app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5175"], credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 //add body parser middleware
 app.use(exp.json());
 //add cookie parser middleware
